ba0d5b9e704f63e22c7c34657592e65ab32a5523
Randall71 <51382079+Randall71@users.noreply.github.com>
2023-03-14 11:57:28 +0000

    feat: Exercise 1

---
+++ src/components/FormInput/FormInput.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function FormInput() {
+  const [word, setWord] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+    setWord("");
+  };
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        minLength={2}
+        maxLength={5}
+        pattern=".{2,5}"
+        required
+        value={word}
+        onChange={(e) => setWord(e.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default FormInput;
